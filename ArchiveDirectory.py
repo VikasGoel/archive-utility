d00e13e43cbb7e65718639d4e2a8326581625dbb
Timothy Ubbens <timothy.ubbens@gmail.com>
2015-09-18 23:31:22 -0400

    Add & integrate encryption stub.  It's now easy to integrate the encryption tool of choice.

--- ArchiveDirectory.py
+++ ArchiveDirectory.py
@@ -12,6 +12,8 @@ class ArchiveDirectory(object):
         string directory expected
     destination -- the destination to archive files from the source
         string directory expected
+    encrypt -- flag to track whether to encrypt files as they are archived.
+        boolean expected
     retention_days -- number of days to retain archived files.
         integer number of days expected
     """
@@ -19,7 +21,7 @@ class ArchiveDirectory(object):
 
     def __init__(self, name, source, destination,
                  action='Move', retention_days=10, recursion_depth=0,
-                 include_pattern='*'):
+                 include_pattern='*', encrypt=False):
         """Return an ArchiveDirectory object
 
         Keyword arguments:
@@ -30,6 +32,7 @@ class ArchiveDirectory(object):
         retention_days --  (default 10)
         recursion_depth --  (default 0)
         include_pattern --  (default '*')
+        encrypt --  (default False)
         """
         self.logger = logging.getLogger('myapp.lib')
         self.logger.debug('ArchiveDirectory Constructor ran')
@@ -41,6 +44,7 @@ class ArchiveDirectory(object):
         self.retention_days = retention_days
         self.recursion_depth = recursion_depth
         self.include_pattern = include_pattern
+        self.encrypt = encrypt
 
         self.logger.debug('name            %r' % self.name)
         self.logger.debug('source          %s' % self.source)
@@ -49,6 +53,7 @@ class ArchiveDirectory(object):
         self.logger.debug('retention days  %r' % self.retention_days)
         self.logger.debug('recursion depth %r' % self.recursion_depth)
         self.logger.debug('include pattern %r' % self.include_pattern)
+        self.logger.debug('encrypt         %r' % self.encrypt)
 
 
     def validate_config(self):
@@ -116,6 +121,7 @@ class ArchiveDirectory(object):
             dest_file_name = os.path.join(recurse_dest, basename)
             alternate_dest_basename = time.strftime('%Y%m%d-%H%M%S-') + basename
             alternate_dest_file_name = os.path.join(recurse_dest,alternate_dest_basename)
+            encrypt_input_file = ''
 
             # Copy or Move
             if self.action.lower() == 'move':
@@ -124,8 +130,10 @@ class ArchiveDirectory(object):
                             'destination file already exists, moving to %s instead',
                             alternate_dest_file_name)
                     shutil.move(source_file_name, alternate_dest_file_name)
+                    encrypt_input_file = alternate_dest_file_name
                 else:
                     shutil.move(source_file_name,dest_file_name)
+                    encrypt_input_file = dest_file_name
             elif self.action.lower() == 'copy':
                 # Copy only files that don't exist in the destination or files
                 # that have changed (more recent last modified timestamp than
@@ -133,11 +141,21 @@ class ArchiveDirectory(object):
                 if ((not os.path.exists(dest_file_name))
                         or (os.stat(source_file_name).st_mtime - os.stat(dest_file_name).st_mtime > 1)):
                     shutil.copy2(source_file_name, dest_file_name)
+                    encrypt_input_file = dest_file_name
             elif self.action.lower() == 'uniquecopy':
                 shutil.copy2(source_file_name, alternate_dest_file_name)
+                encrypt_input_file = alternate_dest_file_name
             else:
                 print 'pick a valid option, dummy!'
                 # you should have run validate_config() function first!
+
+            if self.encrypt == True and len(encrypt_input_file)>0:
+                encrypt_output_file = self.__encrypt_file(encrypt_input_file)
+                self.logger.info('file encrypted from: %s', encrypt_input_file)
+                print encrypt_input_file
+                print 'file encrypted from: ' + encrypt_input_file
+                self.logger.info('file encrypted to:   %s', encrypt_output_file)
+                print 'file encrypted to:   ' + encrypt_output_file
 
         return 'success'
 
@@ -200,6 +218,12 @@ class ArchiveDirectory(object):
             yield 'Source not found: %s' % source_dir
             # Don't cause the exception to stop app execution
             pass
+
+
+    def __encrypt_file(self, input_encrypt_file):
+        # This stub should be completed with your encryption solution of choice
+        # This method should return the encrypted file name
+        return ''
 
 
     @staticmethod
